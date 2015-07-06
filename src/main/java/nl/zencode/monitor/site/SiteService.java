5549d533aee2808288d460b45bd6bfb0da56710f
Koert Zeilstra <koert@zencode.nl>
2015-07-06 19:11:08 +0200

    Made REST service and embedded Glassfish working.

--- src/main/java/nl/zencode/monitor/site/SiteService.java
+++ src/main/java/nl/zencode/monitor/site/SiteService.java
@@ -4,6 +4,7 @@ import javax.annotation.ManagedBean;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,9 +19,18 @@ public class SiteService {
     @GET
     @Path("/all")
     @Produces({"application/xml", "application/json"})
-    public List<String> allSites() {
-        List<String> sites = new ArrayList<String>();
-        sites.add("www.google.com");
+    public List<Site> allSites() {
+        List<Site> sites = new ArrayList<>();
+        Site site = new Site();
+        site.name = "Google";
+        site.url = "http://www.google.com";
+        sites.add(site);
         return sites;
+    }
+
+    @XmlRootElement(name = "site")
+    public static class Site {
+        public String name;
+        public String url;
     }
 }
